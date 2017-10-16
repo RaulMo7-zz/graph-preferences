848b1e2a9d18078745457c5bbc1eabc7f30317e3
RaulMo <remolina7@gmail.com>
2017-10-16 00:15:34 -0500

    firebase implementation

--- app-graph/src/environments/environment.prod.ts
+++ app-graph/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyDVLRwGg64oTKNZAfjB8TbxvFioML3jyWU",
+    authDomain: "alkanza-project.firebaseapp.com",
+    databaseURL: "https://alkanza-project.firebaseio.com",
+    projectId: "alkanza-project",
+    storageBucket: "alkanza-project.appspot.com",
+    messagingSenderId: "204670033764"
+  }
 };
